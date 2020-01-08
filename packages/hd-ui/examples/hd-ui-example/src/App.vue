b8465343853cd9e3c9a77a8f3b8779559e7183fd
upcwangying <upcwangying@126.com>
2020-01-08 17:30:00 +0800

    docs[hd-ui]: add examples

---
+++ packages/hd-ui/examples/hd-ui-example/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <hello title="hello hd ui" />
+    <i class="hd-ui-icon-code"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+i {
+  font-size: 26px;
+  color: red;
+}
+</style>
